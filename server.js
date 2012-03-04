1f751c93683ad3e66d4f04e015d818946035a6a9
Aaron <akbr@akbr.net>
2012-03-04 23:21:22 +0000

    'first commit'

---
+++ server.js
@@ -0,0 +1,15 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+    baseUrl: "lib",
+    nodeRequire: require
+});
+
+var adjudicator;
+
+requirejs(['adjudicator/App'],
+function   (delivery) {
+    adjudicator = delivery;
+});
+
+console.log(adjudicator);

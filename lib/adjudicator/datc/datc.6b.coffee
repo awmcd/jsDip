431d3ae7c5fcb2dd04eb7828e210bddf9390240d
Aaron <akbr@akbr.net>
2012-03-10 07:07:58 +0000

    'through datc 6.b.13'

--- lib/adjudicator/datc/datc.6b.coffee
+++ lib/adjudicator/datc/datc.6b.coffee
@@ -14,3 +14,124 @@ define ->
             unit: [['fra', 'F', 'gas']]
             orders: ['F gas-spa']
             expect: gas: result:true
+            
+        b3:
+            id: '6.B.3'
+            what: 'MOVING WITH WRONG COAST WHEN COAST IS NOT NECESSARY'
+            unit: [['fra', 'F', 'gas']]
+            orders: ['F gas-spa_sc']
+            expect: gas: result:false
+            
+        b4:
+            id: '6.B.4'
+            what: 'SUPPORT TO UNREACHABLE COAST ALLOWED'
+            unit: [['fra', 'F', 'gas'], ['fra', 'F', 'mar'], ['ita', 'F', 'wes']]
+            orders: ['F gas-spa_nc', 'F mar S F gas-spa_nc', 'F wes-spa_sc']
+            expect:
+                gas:
+                    result:true
+                mar:
+                    result:true
+                wes:
+                    result:false
+                    
+        b5:
+            id: '6.B.5'
+            what: 'SUPPORT FROM UNREACHABLE COAST NOT ALLOWED'
+            unit: [['fra', 'F', 'mar'], ['fra', 'F', 'spa_nc'], ['ita', 'F', 'gol']]
+            orders: ['F mar-gol', 'F spa_nc S F mar-gol', 'F gol H']
+            expect:
+                mar:
+                    result:false
+                spa:
+                    result:false
+                gol:
+                    result:true 
+                    
+        b6:
+            id: '6.B.6'
+            what: 'SUPPORT CAN BE CUT WITH OTHER COAST'
+            unit: [['eng', 'F', 'iri'], ['eng', 'F', 'nat'], ['fra', 'F', 'spa_nc'],
+                    ['fra', 'F', 'mid'], ['ita', 'F', 'gol']]
+            orders: ['F iri S F nat-mid', 'F nat-mid', 'F spa_nc S F mid',
+                     'F mid H', 'F gol-spa_nc']
+            expect:
+                mid:
+                    dislodged:true
+                nat:
+                    result:true
+                    
+        b7:
+            id: '6.B.7'
+            what: 'SUPPORTING WITH UNSPECIFIED COAST'
+            unit: [['fra', 'F', 'por'], ['fra', 'F', 'mid'], ['ita', 'F', 'gol'],
+                    ['ita', 'F', 'wes']]
+            orders: ['F por S F mid-spa', 'F mid-spa_nc', 'F gol S F wes-spa_sc',
+                     'F wes-spa_sc']
+            expect:
+                wes:
+                    result:false
+                    
+        b8:
+            id: '6.B.8'
+            what: 'SUPPORTING WITH UNSPECIFIED COAST WHEN ONLY ONE COAST IS POSSIBLE'
+            unit: [['fra', 'F', 'por'], ['fra', 'F', 'gas'], ['ita', 'F', 'gol'],
+                    ['ita', 'F', 'wes']]
+            orders: ['F por S F gas-spa', 'F gas-spa_nc', 'F gol S F wes-spa_sc',
+                     'F wes-spa_sc']
+            expect:
+                por:
+                    result:true
+                wes:
+                    result:false
+                    
+        b9:
+            id: '6.B.9'
+            what: 'SUPPORTING WITH WRONG COAST'
+            unit: [['fra', 'F', 'por'], ['fra', 'F', 'mid'], ['ita', 'F', 'gol'],
+                    ['ita', 'F', 'wes']]
+            orders: ['F por S F gas-spa_nc', 'F mid-spa_sc', 'F gol S F wes-spa_sc',
+                     'F wes-spa_sc']
+            expect:
+                por:
+                    result:false
+                wes:
+                    result:true
+        
+        # I have purposefully left explicitly-ordered incorrect coasts failing,
+        # even though it is not the preferred DATC method.
+        
+        b10:
+            id: '6.B.10'
+            what: 'UNIT ORDERED WITH WRONG COAST'
+            unit: [['fra', 'F', 'spa_sc']]
+            orders: ['F spa_nc-gol']
+            expect:
+                spa: result:false
+        
+        b11:
+            id: '6.B.11'
+            what: 'COAST CAN NOT BE ORDERED TO CHANGE'
+            unit: [['fra', 'F', 'spa_nc']]
+            orders: ['F spa_sc-gol']
+            expect:
+                spa: result:false
+                
+        b12:
+            id: '6.B.12'
+            what: 'ARMY MOVEMENT WITH COASTAL SPECIFICATION'
+            unit: [['fra', 'A', 'gas']]
+            orders: ['A gas-spa_nc']
+            expect:
+                gas: result:true
+                
+        b13:
+            id: '6.B.13'
+            what: 'COASTAL CRAWL NOT ALLOWED'
+            unit: [['tur', 'F', 'bul_sc'], ['tur', 'F', 'con']]
+            orders: ['F bul_sc-con', 'F con-bul_ec']
+            expect:
+                bul:
+                    result:false
+                con:
+                    result:false
--- test-js/spec/datc.js
+++ test-js/spec/datc.js
@@ -16,9 +16,13 @@ describe("DATC Tests", function () {
         var report = function (results, test6a) {
             if (results) {
                 console.log(results);
-            } else {
                 console.log('----- ', test6a.id, ' ', test6a.what);
-                console.log('-------- PASSED!');
+                console.log('-------- FFFFFFFFFFFFFAAIILL!');
+                console.log('-----------------------------');
+                console.log(' ');
+            } else {
+                console.log('----- Passed: ', test6a.id, ' ', test6a.what);
+                console.log(' ');
                 console.log(' ');
             }
         }
@@ -61,7 +65,7 @@ describe("DATC Tests", function () {
             report(results, test);
             expect(results).toEqual(false);
         });
-        
+                
         it('6.A.7: ONLY ARMIES CAN BE CONVOYED', function () {
             var test = tests.a7;
             var results = jsDip.tests.execute(test);
@@ -108,12 +112,16 @@ describe("DATC Tests", function () {
     describe("COASTAL ISSUES", function () {
         var tests;
     
-        var report = function (results, test6b) {
+        var report = function (results, test6a) {
             if (results) {
                 console.log(results);
+                console.log('----- ', test6a.id, ' ', test6a.what);
+                console.log('-------- FFFFFFFFFFFFFAAIILL!');
+                console.log('-----------------------------');
+                console.log(' ');
             } else {
-                console.log('----- ', test6b.id, ' ', test6b.what);
-                console.log('-------- PASSED!');
+                console.log('----- Passed: ', test6a.id, ' ', test6a.what);
+                console.log(' ');
                 console.log(' ');
             }
         }
@@ -135,5 +143,83 @@ describe("DATC Tests", function () {
             report(results, test);
             expect(results).toEqual(false);
         });
+        
+        it('6.B.3: MOVING WITH WRONG COAST WHEN COAST IS NOT NECESSARY', function () {
+            var test = tests.b3;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.4: SUPPORT TO UNREACHABLE COAST ALLOWED', function () {
+            var test = tests.b4;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.5: SUPPORT FROM UNREACHABLE COAST NOT ALLOWED', function () {
+            var test = tests.b5;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.6: SUPPORT CAN BE CUT WITH OTHER COAST', function () {
+            var test = tests.b6;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.7: SUPPORTING WITH UNSPECIFIED COAST', function () {
+            var test = tests.b7;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.8: SUPPORTING WITH UNSPECIFIED COAST WHEN ONLY ONE COAST IS POSSIBLE', function () {
+            var test = tests.b8;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.9: SUPPORTING WITH WRONG COAST', function () {
+            var test = tests.b9;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.10: UNIT ORDERED WITH WRONG COAST', function () {
+            var test = tests.b10;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.11: COAST CAN NOT BE ORDERED TO CHANGE', function () {
+            var test = tests.b11;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.12: ARMY MOVEMENT WITH COASTAL SPECIFICATION', function () {
+            var test = tests.b12;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
+        it('6.B.13: COASTAL CRAWL NOT ALLOWED', function () {
+            var test = tests.b13;
+            var results = jsDip.tests.execute(test);
+            report(results, test);
+            expect(results).toEqual(false);
+        });
+        
     });
 });
